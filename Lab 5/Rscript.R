51544aa2c92e9564f3ccc9346aaba8e3f09c0345
Jacob Johnsson <jacob.johnsson1992@gmail.com>
2018-02-21 10:08:56 +0100

    Add files via upload

--- Lab 5/Rscript.R
+++ Lab 5/Rscript.R
@@ -6,11 +6,28 @@ plotresult <- function(file, start = 1){
   plot(data, type = 'l')
 }
 
+loopfunction <- function(n){
+  totalmean <- 1:n
+  for(index in 1:n){
+    system("java -cp bin sorter.Lab infil.txt utfil.txt 600")
+    plotresult("resources/utfil.txt", 200)
+    data <- read.csv("resources/utfil.txt", header = T)
+    data <- data[200:nrow(data),]
+    totalmean[index] <- mean(data[,2])
+  }
+  #fullhax för att få en linje av verkliga medelvärdet
+  truemean <- 1:n
+  for(index in 1:n){
+    truemean[index] <- mean(totalmean)
+  }
+  plot(totalmean, type = 'l')
+  lines(truemean, type = 'l')
+}
 system("java -cp bin sorter.Lab infil.txt utfil.txt 600")
 plotresult("resources/utfil.txt", 200)
+loopfunction(50)
 
 pdf("result1.pdf")
 plotresult("resources/utfil.txt", 200)
-#plot(data, typ = 'l')
 
 dev.off()
