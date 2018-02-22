427f24f02b8e76df55ca2ad9361036cc4e2bb34b
Jacob Johnsson <jacob.johnsson1992@gmail.com>
2018-02-22 13:00:22 +0100

    Add files via upload

--- Lab 5/Rscript.R
+++ Lab 5/Rscript.R
@@ -1,7 +1,7 @@
 
 # function for plotting data
 plotresult <- function(file, start = 1){
-  data <- read.csv("resources/utfil.txt", header = T)
+  data <- read.csv(file, header = T)
   data <- data[start:nrow(data),]
   plot(data, type = 'l')
 }
@@ -10,24 +10,29 @@ plotresult <- function(file, start = 1){
 loopfunction <- function(n){
   vectorOfMeans <- 1:n
   for(index in 1:n){
-    system("java -Xint -cp bin sorter.Lab infil.txt utfil.txt 600")
+    system("java -cp bin sorter.LabEgenSort data1.txt utfil.txt 600")
     data <- read.csv("resources/utfil.txt", header = T)
-    data <- data[200:nrow(data),]
+    data <- data[150:nrow(data),]
     vectorOfMeans[index] <- mean(data[,2])
   }
   plot(vectorOfMeans, type = 'l')
   return(vectorOfMeans)
 }
-system("java -Xint -cp bin sorter.Lab infil.txt utfil.txt 600")
-vectorOfMeans <- loopfunction(10)
-#plot(vectorOfMeans, type = 'l')
+
+## Dessa två används då det ska köras EN gång!
+#system("java -cp bin sorter.LabEgenSort data1.txt utfil.txt 600")
+#plotresult("resources/utfil.txt", 1)
+
+vectorOfMeans <- loopfunction(100)
+plot(vectorOfMeans, type = 'l')
 
 source("/usr/local/cs/EDAA35/R_resources.R")
 confIntValue <- confidenceInterval(vectorOfMeans, confidenceLevel = 0.95)
 
-averageOfMeans <- mean(vectorOfMeans) 
+averageOfMeans <- mean(vectorOfMeans)
 
 pdf("result1.pdf")
-plotresult("resources/utfil.txt", 200)
+plot(vectorOfMeans, type = 'l')
+#plotresult("resources/utfil.txt", 1)
 
 dev.off()
